db9dc00294245849223a0ef329046bb7985c3c47
Jon Stanley <jonstanley@gmail.com>
2017-07-12 16:51:37 -0400

    Initial changes - add hello

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,16 @@
 from flask import Flask
 application = Flask(__name__)
 
+
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route('/world')
+def world():
+    return 'World Hello'
+
 
 if __name__ == "__main__":
     application.run()
